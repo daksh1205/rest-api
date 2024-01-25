85a8944e71f8324ccb8cbb8a2def42b23fdb125e
daksh <dakshyt1205@gmail.com>
2024-01-25 21:59:38 +0530

    update: creation of model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,6 +4,7 @@ class User {
   final String phone;
   final String cell;
   final String nat;
+  final UserName name;
 
   User({
     required this.gender,
@@ -11,5 +12,18 @@ class User {
     required this.phone,
     required this.cell,
     required this.nat,
+    required this.name,
+  });
+}
+
+class UserName {
+  final String title;
+  final String first;
+  final String last;
+
+  UserName({
+    required this.title,
+    required this.first,
+    required this.last,
   });
 }
