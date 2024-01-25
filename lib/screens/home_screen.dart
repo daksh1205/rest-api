d9a97f2adf5019af1626fa6b7ea43e39e13f34d5
daksh <dakshyt1205@gmail.com>
2024-01-25 23:15:18 +0530

    update: creation of services

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:rest_api/models/user.dart';
+import 'package:rest_api/models/user_name.dart';
+import 'package:rest_api/services/user_api.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,6 +17,12 @@ class _HomeScreenState extends State<HomeScreen> {
   List<User> users = [];
 
   @override
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -25,43 +33,23 @@ class _HomeScreenState extends State<HomeScreen> {
         itemBuilder: (context, index) {
           final user = users[index];
           final email = user.email;
-          final colr = user.gender == 'male' ? Colors.blue : Colors.green;
+          // final colr = user.gender == 'male' ? Colors.blue : Colors.green;
           return ListTile(
             title: Text(
-              email,
-              style: TextStyle(color: colr),
+              user.fullName,
+              // style: TextStyle(color: colr),
             ),
             subtitle: Text(user.phone),
           );
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: fetchUsers,
-      ),
     );
   }
 
-  void fetchUsers() async {
-    print('fetchUsers called');
-    const url = 'https://randomuser.me/api/?results=100';
-    final uri = Uri.parse(url);
-    await http.get(uri);
-    final response = await http.get(uri);
-    final body = response.body;
-    final json = jsonDecode(body);
-    final results = json['results'] as List<dynamic>;
-    final transformed = results.map((e) {
-      return User(
-        cell: e['cell'],
-        email: e['email'],
-        gender: e['gender'],
-        nat: e['nat'],
-        phone: e['phone'],
-      );
-    }).toList();
+  Future<void> fetchUsers() async {
+    final response = await UserApi.fetchUsers();
     setState(() {
-      users = transformed;
+      users = response;
     });
-    print('fetchUsers completed');
   }
 }
